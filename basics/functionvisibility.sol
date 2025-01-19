6f7728a9a810acf58824dad14e6f8b4d4f17c7e9
mxzyy <hilmyp53@Gmail.com>
2025-01-19 22:26:58 +0700

    Add Storage + Function Visibility update

--- basics/functionvisibility.sol
+++ basics/functionvisibility.sol
@@ -3,10 +3,7 @@ pragma solidity ^0.8.0;
 
 // Contract utama
 contract MainContract {
-    // Variable private yang hanya bisa diakses di dalam contract ini
     uint private privateData;
-    
-    // Variable public yang bisa diakses dari mana saja
     uint public publicData;
     
     constructor() {
@@ -14,75 +11,43 @@ contract MainContract {
         publicData = 200;
     }
     
-    // Private function - hanya bisa dipanggil di dalam contract ini
     function privateFunction() private view returns(uint) {
         return privateData;
     }
     
-    // Public function - bisa dipanggil dari mana saja
     function publicFunction() public view returns(uint) {
         return privateFunction() + publicData;
     }
-    
-    // Internal function - bisa diakses oleh contract ini dan turunannya
+
     function internalFunction() internal view returns(uint) {
         return privateData * 2;
     }
 
-    // External function - hanya bisa dipanggil dari luar contract
-    function externalFunction() external view returns(uint) {
-        return publicData * 3;
+    function callPrivateFunc() public  view  returns (uint) {
+        return privateFunction();
     }
 
-    // Function ini akan error karena mencoba memanggil external function dari dalam contract
-    // function testExternalCall() public view returns(uint) {
-    //     return this.externalFunction(); // Harus menggunakan 'this' jika ingin memanggil external function
-    //     return externalFunction(); // Error! Tidak bisa langsung memanggil external function
-    // }
-}
-
-// Contract turunan
-contract ChildContract is MainContract {
-    function getInternalCalculation() public view returns(uint) {
+    function callInternalFunc() public  view  returns (uint) {
         return internalFunction();
     }
 
-    // External function baru di contract turunan
-    function childExternalFunction() external pure returns(string memory) {
-        return "Called from child contract";
+    function externalFunction() external view returns(uint) {
+        return publicData * 3;
     }
 }
 
-// Contract terpisah untuk menunjukkan cara memanggil external function
-contract CallerContract {
+contract Caller {
     MainContract mainContract;
-    ChildContract childContract;
-    
-    constructor(address _mainContract, address _childContract) {
+    constructor(address _mainContract) {
         mainContract = MainContract(_mainContract);
-        childContract = ChildContract(_childContract);
     }
-    
-    // Cara memanggil public function
-    function callPublicFunction() public view returns(uint) {
+
+    function callPublicFunction() public view returns (uint) {
         return mainContract.publicFunction();
     }
 
-    // Cara memanggil external function
-    function callExternalFunction() public view returns(uint) {
+    function callExternalFunction() public view returns (uint) {
         return mainContract.externalFunction();
     }
-
-    // Cara memanggil external function dari child contract
-    function callChildExternal() public view returns(string memory) {
-        return childContract.childExternalFunction();
-    }
-
-    // Fungsi untuk mendemonstrasikan multiple calls
-    function multipleExternalCalls() public view returns(uint) {
-        // Bisa memanggil external function berkali-kali
-        uint result1 = mainContract.externalFunction();
-        uint result2 = mainContract.externalFunction();
-        return result1 + result2;
-    }
 }
+
---
+++ simple-storage/simpleStorage.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.26;
+
+contract simpleStorage {
+    uint256 favouriteNumber;
+
+    struct person {
+        string name;
+        uint256 favouriteNumber;
+    }
+
+    person[] public listPersons;
+
+    mapping (string => uint256) public nameToFavNumber;
+
+    function store(uint256 _favouriteNumber) public {
+        favouriteNumber = _favouriteNumber;
+    }
+
+    function retriveNumber() public view returns (uint256) {
+        return favouriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favouriteNum) public {
+        listPersons.push(person(_name, _favouriteNum));
+        nameToFavNumber[_name] = _favouriteNum;
+    }
+
+}
