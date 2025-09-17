9e9079e65c3f735a7da196d0c9436c87adb8f3b5
mxzyy <hilmyp53@gmail.com>
2025-09-17 22:27:54 +0700

    feat: :construction_worker: Add Integration

---
+++ foundry-fundamental/fund-me/test/integration/Integration.t.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {console} from "forge-std/console.sol";
+import {MockV3Aggregator} from "../mock/MockV3Aggregator.sol";
