35595d8910c7ac3ceb5a9ae77e7331542b1ef0e0
mxzyy <hilmyp53@gmail.com>
2025-09-18 20:55:27 +0700

    feat: :zap: Add integration

--- foundry-fundamental/fund-me/test/integration/Integration.t.sol
+++ foundry-fundamental/fund-me/test/integration/Integration.t.sol
@@ -1,6 +1,54 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {Test} from "forge-std/Test.sol";
-import {console} from "forge-std/console.sol";
-import {MockV3Aggregator} from "../mock/MockV3Aggregator.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe_Fund_InteractionContract, FundMe_Withdraw_InteractionContract} from "../../script/Interaction.s.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {StdCheats} from "forge-std/StdCheats.sol";
+import {ZkSyncChainChecker} from "lib/foundry-devops/src/ZkSyncChainChecker.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+
+contract InteractionTests is ZkSyncChainChecker, StdCheats, Test {
+    FundMe public fundme;
+    HelperConfig public helperConfig;
+
+    // @notice constants
+    uint256 public constant SEND_VALUE = 0.1 ether;
+    uint256 public constant STARTING_USER_BALANCE = 10 ether;
+    address public constant GAS_PRICE = 1;
+
+    address public USER = address(1);
+
+    function setUp() external skipZkSync {
+        if (!isZkSyncChain()) {
+            // why only zk
+            DeployFundMe deployer = new DeployFundMe();
+            (fundme, helperConfig) = deployer.run();
+        } else {
+            helperConfig = new HelperConfig();
+            fundMe = new FundMe(helperConfig.getConfigByChainId(block.chainid).priceFeed);
+        }
+
+        vm.deal(USER, STARTING_USER_BALANCE);
+    }
+
+    function testUserCanFundAndWithdraw() public skipZkSync {
+        uint256 preUserBalance = address(USER).balance;
+        uint256 preOwnerBalance = address(fundme.getOwner()).balance;
+        uint256 originalFundMeBalance = address(fundme).balance;
+
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+
+        FundMe_Withdraw_InteractionContract withdrawer = new FundMe_Withdraw_InteractionContract(address(fundme));
+        withdrawer.FundMe_Withdraw_InteractionContract(address(fundMe)); // why not owner
+
+        uint256 afterUserBalance = address(USER).balance;
+        uint256 afterOwnerBalance = address(fundme.getOwner()).balance;
+
+        assert(address(fundme).balance == 0);
+        assertEq(afterUserBalance + SEND_VALUE, preUserBalance);
+        assertEq(afterOwnerBalance + SEND_VALUE + originalFundMeBalance, preOwnerBalance);
+    }
+}
