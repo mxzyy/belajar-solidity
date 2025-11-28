1380f909ca3bb4a39658f654ab2c2408ced937e3
mxzyy <hilmyp53@gmail.com>
2025-11-28 08:59:09 +0700

    feat: 🎉 Clears up unit test for Raffle contract

--- foundry-fundamental/lottery/script/DeployRaffle.s.sol
+++ foundry-fundamental/lottery/script/DeployRaffle.s.sol
@@ -2,7 +2,10 @@
 pragma solidity ^0.8.20;
 
 import {Script} from "forge-std/Script.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 import {Raffle} from "../src/Raffle.sol";
+import {CreateSubscription, AddConsumer, FundSubscription} from "./Interactions.s.sol";
+import {RaffleDeployerLib} from "../src/RaffleDeployerLib.sol";
 
 /**
  * @title A script for deploying Raffle contract
@@ -11,17 +14,26 @@ import {Raffle} from "../src/Raffle.sol";
  * @dev This implements the Script from forge-std
  */
 contract DeployRaffle is Script {
-    function run() external {
-        uint256 subscriptionId = vm.envUint("SUBSCRIPTION_ID");
-        bytes32 gasLane = vm.envBytes32("GAS_LANE");
-        uint256 interval = vm.envUint("INTERVAL");
-        uint256 entranceFee = vm.envUint("ENTRANCE_FEE");
-        uint256 rawcallbackGasLimit = vm.envUint("CALLBACK_GAS_LIMIT");
-        require(rawcallbackGasLimit <= type(uint32).max, "CALLBACK_GAS_LIMIT too large");
-        uint32 callbackGasLimit = uint32(rawcallbackGasLimit);
-        address vrfCoordinatorV2 = vm.envAddress("VRF_COORDINATOR");
+    function run() external returns (Raffle, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig();
+        AddConsumer addConsumer = new AddConsumer();
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+        config = RaffleDeployerLib.prepareConfig(config);
+        uint256 subscriptionId = config.subscriptionId;
+        bytes32 gasLane = config.gasLane;
+        uint256 interval = config.automationUpdateInterval;
+        uint256 entranceFee = config.raffleEntranceFee;
+        uint256 rawcallbackGasLimit = config.callbackGasLimit;
+        uint32 callbackGasLimit = RaffleDeployerLib.prepareRawCallbackGasLimit(rawcallbackGasLimit);
+        address vrfCoordinatorV2 = config.vrfCoordinatorV2_5;
         vm.startBroadcast();
-        new Raffle(subscriptionId, gasLane, interval, entranceFee, callbackGasLimit, vrfCoordinatorV2);
+        Raffle raffleContract =
+            new Raffle(subscriptionId, gasLane, interval, entranceFee, callbackGasLimit, vrfCoordinatorV2);
         vm.stopBroadcast();
+
+        addConsumer.addConsumer(
+            address(raffleContract), config.vrfCoordinatorV2_5, config.subscriptionId, config.account
+        );
+        return (raffleContract, helperConfig);
     }
 }
